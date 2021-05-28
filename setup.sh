b9d4b04511f5901d0bebeac8477b2f63fcfbedbe
Spyder-robot <84974334+Spyder-robot@users.noreply.github.com>
2021-05-28 16:51:43 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,6 +11,20 @@ sudo raspi-config nonint do_camera 0
 sudo apt-get install -y proftpd
 sudo apt-get install -y avrdude
 sudo apt-get install -y i2c-tools
-curl -sS https://raw.githubusercontent.com/adafruit/Raspberry-Pi-Installer-Scripts/master/i2samp.sh | bash
+curl -sS https://raw.githubusercontent.com/adafruit/Raspberry-Pi-Installer-Scripts/master/i2samp.sh -y | bash
+sudo apt install -y python3-pip
+
+sudo pip3 install -y rpi.gpio
+sudo pip3 install -y Pillow
+sudo apt-get install -y libopenjp2-7
+sudo apt-get install -y libtiff5
+sudo pip3 install -y spidev
+sudo pip3 install -y numpy
+sudo pip3 install -y st7789
+sudo pip3 install -y pyserial
+sudo pip3 install -y smbus2
+sudo pip3 install -y pygame==2.0.0
+sudo apt-get install -y git curl libsdl2-mixer-2.0-0 libsdl2-image-2.0-0 libsdl2-2.0-0 libsdl2-ttf-2.0-0
+sudo apt-get install -y python-picamera python3-picamera
 
 sudo apt-get install fonts-dejavu and fonts-dejavu-core and fonts-font-awesome
