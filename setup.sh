677f71597b3dc6c1cce71bab1265b0ce0a3ae67c
Spyder-robot <84974334+Spyder-robot@users.noreply.github.com>
2021-05-28 16:33:16 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,3 +7,10 @@ sudo raspi-config nonint do_spi 0
 sudo raspi-config nonint do_i2c 0
 sudo raspi-config nonint do_serial 2
 sudo raspi-config nonint do_camera 0
+
+sudo apt-get install -y proftpd
+sudo apt-get install -y avrdude
+sudo apt-get install -y i2c-tools
+curl -sS https://raw.githubusercontent.com/adafruit/Raspberry-Pi-Installer-Scripts/master/i2samp.sh | bash
+
+sudo apt-get install fonts-dejavu and fonts-dejavu-core and fonts-font-awesome
