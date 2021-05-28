3c193f6a6ba54c9e0a2f13113bdcb832323b89a7
Spyder-robot <84974334+Spyder-robot@users.noreply.github.com>
2021-05-28 16:21:52 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,3 +2,8 @@
 
 sudo apt-get update
 sudo apt-get upgrade -y
+
+sudo raspi-config nonint do_spi 0
+sudo raspi-config nonint do_i2c 0
+sudo raspi-config nonint do_serial 2
+sudo raspi-config nonint do_camera 0
